b750c8e69dcda605ca3e809cbddb2970569f5790
Artur <artur.jarosiewicz6@gmail.com>
2023-05-02 18:35:13 +0200

    Create edit feature and update list

--- gui.py
+++ gui.py
@@ -2,7 +2,8 @@ import functions
 import PySimpleGUI as sg
 
 layout = [[sg.Text('Type in a TO-DO')],
-        [sg.InputText(key='todo'), sg.Button('Add')]]
+        [sg.InputText(key='todo'), sg.Button('Add')],
+        [sg.Listbox(values=functions.get_todos(), enable_events=True, size=(45, 10), key='todos'), sg.Button('Edit')]]
 
 window = sg.Window('My To-Do app', layout, font=('Ariel', 16))
 
@@ -15,6 +16,20 @@ while True:
                         todos = functions.get_todos()
                         todos.append(values['todo'] + '\n')
                         functions.write_todos(todos)
+                        window['todos'].update(values=todos)
+                case 'Edit':
+                        todo_to_edit = values['todos'][0]
+                        new_todo = values['todo']
+
+                        todos = functions.get_todos()
+                        index = todos.index(todo_to_edit)
+                        todos[index] = new_todo
+
+                        functions.write_todos(todos)
+                        window['todos'].update(values=todos)
+                case 'todos':
+                        window['todo'].update(value=values['todos'][0])
+
                 case sg.WINDOW_CLOSED:
                         break
 window.close()
